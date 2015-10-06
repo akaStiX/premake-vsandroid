87563eafde1180687d4895d6ca0eff234fa7b02c
StiX <stix.dima@gmail.com>
2015-10-06 23:37:37 +0200

    - added filters generator for androidproj

--- vsandroid.lua
+++ vsandroid.lua
@@ -10,16 +10,28 @@
 	local p = premake
 	local vstudio = p.vstudio
 	local android = p.modules.vsandroid
+	local project = p.project
+	local tree = p.tree
 
+
+	function android.generatePackagingProj(prj)
+		p.generate(prj, ".androidproj", p.modules.vsandroid_packaging.generatePackaging)
+		
+		-- Skip generation of empty user files
+		local user = p.capture(function() vstudio.vc2010.generateUser(prj) end)
+		if #user > 0 then
+			p.generate(prj, ".androidproj.user", function() p.outln(user) end)
+		end
+		
+		-- Only generate a filters file if the source tree actually has subfolders
+		if tree.hasbranches(project.getsourcetree(prj)) then
+			premake.generate(prj, ".vcxproj.filters", vstudio.vc2010.generateFilters)
+		end
+	end
 	
 	function android.generateProject(prj)
 		if prj.kind == "Packaging" then
-			p.generate(prj, ".androidproj", p.modules.vsandroid_packaging.generatePackaging)
-			
-			local user = p.capture(function() vstudio.vc2010.generateUser(prj) end)
-			if #user > 0 then
-				p.generate(prj, ".androidproj.user", function() p.outln(user) end)
-			end
+			android.generatePackagingProj(prj)
 		else
 			vstudio.vs2010.generateProject(prj)
 		end
