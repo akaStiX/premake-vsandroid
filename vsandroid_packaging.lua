50f1cc6f1ff5fa8e12200f2284c908f2b56ea338
StiX <stix.dima@gmail.com>
2017-07-06 21:50:15 +0200

    Update vsandroid_packaging.lua
    Fixed scope

--- vsandroid_packaging.lua
+++ vsandroid_packaging.lua
@@ -243,7 +243,7 @@
 	end
 	
 	function pack.androidAppLibName(cfg)
-		vc2010.element("AndroidAppLibName", nil, cfg.applibname)
+		vc2010.element("AndroidAppLibName", nil, prj.applibname)
 	end
 	
 	function pack.applicationName(cfg)
